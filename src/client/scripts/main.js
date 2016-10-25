3974725b87d720b9ac7368b638c4640e81a0a702
nzkks <kkshanthosh@gmail.com>
2016-10-25 18:49:30 +1300

    Added bottom Call To Action section

--- src/client/scripts/main.js
+++ src/client/scripts/main.js
@@ -4,3 +4,24 @@ import appStarter from './app-starter';
 $(function() {
   appStarter();
 });
+
+$(document).ready(function() {
+  var hidden = true;
+  $('.download-btn').click(function(e) {
+    e.preventDefault();
+    if (hidden){
+      $(this).next('.download-files-container').fadeToggle(200, function(){hidden = false;});
+    }
+  });
+
+  $('html').click(function() {
+    if (!hidden) {
+      $('.download-files-container').fadeOut();
+      hidden=true;
+    }
+  });
+
+  $('.download-files-container').click(function(event) {
+    event.stopPropagation();
+  });
+});
