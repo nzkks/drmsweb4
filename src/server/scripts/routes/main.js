b28af259a3da7aef39942fbff286cb3564c41063
nzkks <kkshanthosh@gmail.com>
2016-11-01 22:52:36 +1300

    Added more into skills section

--- src/server/scripts/routes/main.js
+++ src/server/scripts/routes/main.js
@@ -7,16 +7,15 @@ let title = 'Drmsweb4';
 router.get('/', function(req, res) {
   res.render('index',
     {title});
-}
-);
+});
+/*
 router.get('/about', function(req, res) {
   res.render('about',
     {title});
-}
-);
+});
 router.get('/contact', function(req, res) {
   res.render('contact',
     {title});
-}
-);
+});
+*/
 export default router;
