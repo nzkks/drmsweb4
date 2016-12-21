88639c0cc8179005a0b93313dcc1d72b8e4adf41
nzkks <kkshanthosh@gmail.com>
2016-12-21 19:25:00 +1300

    Added lazy load script mainly for images

--- src/client/scripts/main.js
+++ src/client/scripts/main.js
@@ -41,6 +41,54 @@ $(document).ready(function() {
   // if(!Modernizr.svg){var i=document.getElementsByTagName('img'),j,y;for(j=i.length;j--;){y=i[j].src;if(y.match(/svg$/)){i[j].src=y.slice(0,-3)+'png';}}}
   // END: Fall Png images to Svg images
 
+  // BEGIN: Lazy load images
+
+  // This line is nothing to do with lazyload.
+  // By default .lazy class style set to display: none.
+  // Only when JavaScript is present then we are showing the elements.
+  $('.lazy').show();
+
+  var lazy = [];
+  registerListener('load', setLazy);
+  registerListener('load', lazyLoad);
+  registerListener('scroll', lazyLoad);
+  registerListener('resize', lazyLoad);
+  function setLazy(){
+    lazy = document.getElementsByClassName('lazy');
+  }
+  function lazyLoad(){
+    for(var i=0; i<lazy.length; i++){
+      if(isInViewport(lazy[i])){
+        if (lazy[i].getAttribute('data-src')){
+          lazy[i].src = lazy[i].getAttribute('data-src');
+          lazy[i].removeAttribute('data-src');
+        }
+      }
+    }
+    cleanLazy();
+  }
+  function cleanLazy(){
+    lazy = Array.prototype.filter.call(lazy, function(l){ return l.getAttribute('data-src');});
+  }
+  function isInViewport(el){
+    var rect = el.getBoundingClientRect();
+    return (
+      rect.bottom >= 0 &&
+      rect.right >= 0 &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.left <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+  function registerListener(event, func) {
+    if (window.addEventListener) {
+      window.addEventListener(event, func);
+    } else {
+      window.attachEvent('on' + event, func);
+    }
+  }
+
+  // END: Lazy load images
+
   // BEGIN: Local page links
 
   $.easing.elasout = function(x, t, b, c, d) {
